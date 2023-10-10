a555161d22d1899a814650c037388a0f355198d8
Anas@bairuhaTech <anas@bairuhatech.com>
2023-10-10 14:54:15 +0530

    updated-sass

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import WebsiteHeader from "../../component/websiteHeader";
 import WebsiteFooter from "../../component/websiteFooter";
+import HomeScreen from "../homeScreen";
 
 function Home() {
   return (
     <>
       <WebsiteHeader />
+      <HomeScreen />
       <WebsiteFooter />
     </>
   );
---
+++ src/app/homeScreen/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.scss";
+function HomeScreen() {
+  return (
+    <>
+      <div className="homeScreen-Box1">HomeScreen</div>
+    </>
+  );
+}
+
+export default HomeScreen;
