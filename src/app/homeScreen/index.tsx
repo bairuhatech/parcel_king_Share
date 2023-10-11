c765933e44de523959fa935833063200ceaa557d
shahin <shahin@semikolen.com>
2023-10-11 20:42:09 +0530

    update

--- src/app/homeScreen/index.tsx
+++ src/app/homeScreen/index.tsx
@@ -4,12 +4,14 @@ import WebsiteFooter from "../../component/websiteFooter";
 import Block1 from "./block1";
 import Block3 from "./block3";
 import Block4 from "./block4";
+import Block2 from "./block2";
 
 function Home() {
   return (
     <>
       <WebsiteHeader />
       <Block1 />
+      <Block2/>
       <Block3 />
       <Block4 />
       <WebsiteFooter />
