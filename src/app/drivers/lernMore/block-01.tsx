918401b59aa2e35a95719f5cfb1b4ceb5e72dbfe
Sahal <sahal@bairuhatech.com>
2023-10-13 14:08:18 +0400

    updated

--- src/app/drivers/lernMore/block-01.tsx
+++ src/app/drivers/lernMore/block-01.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 import "../styles.scss";
-import LastMile from "../../../asset/image/lastmile2.jpg";
-import BusinessBlock1 from "../../business/component/business-Block1";
-import BusinessBlock3 from "../../business/component/business-Block3";
 const DriversBlock1 = () => {
   return <></>;
 };
